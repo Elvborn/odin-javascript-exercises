9de07297f579b7c6d9a3fef72c407c21e85d3670
Elvborn <ralleejensen@gmail.com>
2024-11-16 10:00:48 +0100

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str
+        .toLowerCase()
+        .split("")
+        .filter(value => (value >= "a" && value <= "z") || (value >= "0" && value <= "9"))
+        .join("");
 
+    const reversedString = str.split('').reverse().join('');
+
+    return str === reversedString;
 };
 
 // Do not edit below this line

ff3731d7765673b44c443798f273da1bfcc7ade9
Elvborn <ralleejensen@gmail.com>
2024-11-16 09:26:26 +0100

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, val) => {
+    return total += val;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, val) => {
+    return total *= val;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+
+  if(num <= 1) return 1;
+
+  for (let i = 2; i <= num; i++) 
+      total = total * i; 
+
+  return total; 
 };
 
 // Do not edit below this line

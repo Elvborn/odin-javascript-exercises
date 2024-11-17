e62862ce898d2ea01cf160c33586163c6e440534
Elvborn <ralleejensen@gmail.com>
2024-11-17 08:28:10 +0100

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+function getAge(birth, death){
+    if(!death)
+        death = new Date().getFullYear();
 
+    return death - birth;
+}
+
+const findTheOldest = function(people) {
+    return people.reduce((prev, current) => {
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        let prevAge = getAge(prev.yearOfBirth, prev.yearOfDeath);
+
+        return currentAge > prevAge ? current : prev;
+    });
 };
 
 // Do not edit below this line

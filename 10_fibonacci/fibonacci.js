28c5b96c15f539a8e3b7e4051af6aa7b43e59045
Elvborn <ralleejensen@gmail.com>
2024-11-17 07:33:34 +0100

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS";
+    if(num == 0) return 0;
+    if(num <= 2) return 1;
 
+    let current = 0;
+    let prev1 = 1;
+    let prev2 = 1;
+
+    for(let i=3; i <= num; i++){
+        current = prev1 + prev2;
+
+        prev1 = prev2;
+        prev2 = current;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
